7e396d415c725d5e9a98487673741ad138652292
Henry Lv <henry.lv206@gmail.com>
2017-11-20 16:20:17 +0800

    add etcd ip
    convert $ETCD_HOST to IP

--- root/launch.sh
+++ root/launch.sh
@@ -5,6 +5,17 @@ set -e
 # add debug
 [[ -n "$DEBUG" ]] && set -x
 
+etcdhost=`nslookup $ETCD_HOST 172.30.0.1 | tail -n 1 | grep 172 | cut -d ' ' -f 3`                                                            
+while [ -z "$etcdhost" ]                                                                                                                    
+do                                                                                                                                          
+  echo "etcdhost is null"   
+  sleep 1  
+  etcdhost=`nslookup $ETCD_HOST 172.30.0.1 | tail -n 1 | grep 172 | cut -d ' ' -f 3`                                                           
+done                                                                                                                                        
+                                                                                                                                            
+echo etcdhost: $etcdhost                                                                                                                    
+export ETCD_HOST=$etcdhost
+
 # allow the container to be started with `--user`
 if [ "$1" = 'rabbitmq-server' -a "$(id -u)" = '0' ]; then
 	chown -R rabbitmq:rabbitmq ${HOME}

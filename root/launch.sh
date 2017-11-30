4910fc24a457f251c12b7c75ba186c8e4335097b
Henry Lv <henry.lv206@gmail.com>
2017-11-30 11:19:10 +0800

    check etcd status
    check etcd status

--- root/launch.sh
+++ root/launch.sh
@@ -5,6 +5,7 @@ set -e
 # add debug
 [[ -n "$DEBUG" ]] && set -x
 
+# get etcd ip
 etcdhost=`nslookup $ETCD_HOST 172.30.0.1 | tail -n 1 | grep 172 | cut -d ' ' -f 3`                                                            
 while [ -z "$etcdhost" ]                                                                                                                    
 do                                                                                                                                          
@@ -15,6 +16,16 @@ done
                                                                                                                                             
 echo etcdhost: $etcdhost                                                                                                                    
 export ETCD_HOST=$etcdhost
+
+# check etcd status
+wget --spider -T 1 $etcdhost:4001/version
+while [ $? -ne 0 ]
+do
+  sleep 1
+  wget --spider -T 1 $etcdhost:4001/version
+done
+
+echo "etcd ip is OK"
 
 # allow the container to be started with `--user`
 if [ "$1" = 'rabbitmq-server' -a "$(id -u)" = '0' ]; then

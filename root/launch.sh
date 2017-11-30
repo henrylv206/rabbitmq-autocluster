8b5f1ce2672a60eda27aaa41fe4e0704b466a7d3
Henry Lv <henry.lv206@gmail.com>
2017-11-30 11:45:40 +0800

    check etcd host and port

--- root/launch.sh
+++ root/launch.sh
@@ -5,27 +5,33 @@ set -e
 # add debug
 [[ -n "$DEBUG" ]] && set -x
 
-# get etcd ip
-etcdhost=`nslookup $ETCD_HOST 172.30.0.1 | tail -n 1 | grep 172 | cut -d ' ' -f 3`                                                            
-while [ -z "$etcdhost" ]                                                                                                                    
-do                                                                                                                                          
-  echo "etcdhost is null"   
-  sleep 1  
-  etcdhost=`nslookup $ETCD_HOST 172.30.0.1 | tail -n 1 | grep 172 | cut -d ' ' -f 3`                                                           
-done                                                                                                                                        
+# check env: ETCD_HOST
+if [ $ETCD_HOST ]; then
+  echo "check etcd host"
+  
+  # get etcd ip
+  etcdhost=`nslookup $ETCD_HOST 172.30.0.1 | tail -n 1 | grep 172 | cut -d ' ' -f 3`                                                            
+  while [ -z "$etcdhost" ]                                                                                                                    
+  do                                                                                                                                          
+    echo "etcdhost is null"   
+    sleep 1  
+    etcdhost=`nslookup $ETCD_HOST 172.30.0.1 | tail -n 1 | grep 172 | cut -d ' ' -f 3`                                                           
+  done                                                                                                                                        
                                                                                                                                             
-echo etcdhost: $etcdhost                                                                                                                    
-export ETCD_HOST=$etcdhost
+  echo etcdhost: $etcdhost                                                                                                                    
+  export ETCD_HOST=$etcdhost
 
-# check etcd status
-wget --spider -T 1 $etcdhost:4001/version
-while [ $? -ne 0 ]
-do
-  sleep 1
-  wget --spider -T 1 $etcdhost:4001/version
-done
+  # check etcd status
+  etcdport=${ETCD_PORT:=4001}
+  wget --spider -T 1 $etcdhost:$etcdport/version
+  while [ $? -ne 0 ]
+  do
+    sleep 1
+    wget --spider -T 1 $etcdhost:$etcdport/version
+  done
 
-echo "etcd ip is OK"
+  echo "etcd ip is OK"
+fi
 
 # allow the container to be started with `--user`
 if [ "$1" = 'rabbitmq-server' -a "$(id -u)" = '0' ]; then
